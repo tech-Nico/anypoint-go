ebb501e60bff755fa0b61c56c55b29f9c776bd80
Nico Balestra <nico.balestra@mulesoft.com>
2017-06-04 01:20:03 +0100

    Work in progress to implement findOrganization

---
+++ rest/accessManagement.go
@@ -0,0 +1,173 @@
+package rest
+
+import (
+	"github.com/dghubble/sling"
+	"log"
+	"os"
+	"net/http"
+	"crypto/tls"
+	"io/ioutil"
+	"encoding/json"
+)
+
+type Auth struct {
+	client *http.Client
+	uri    string
+	Token  string
+}
+
+type LoginPayload struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+type AuthToken struct {
+	BearerToken string `json:"access_token,omitempty"`
+}
+
+const (
+	LOGIN     string = "/accounts/login"
+	ME        string = "/accounts/api/me"
+	HIERARCHY string = "/accounts/api/hierarchy"
+)
+
+func NewAuth(uri, username, password string) *Auth {
+	transCfg := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true}, // ignore expired SSL certificates
+	}
+	client := &http.Client{Transport: transCfg}
+	return &Auth{
+		client,
+		uri,
+		login(client, uri, username, password),
+	}
+}
+
+// Login the given user and return the bearer Token
+func login(httpClient *http.Client, uri, pUsername, pPassword string) string {
+
+	body := LoginPayload{
+		Username: pUsername,
+		Password: pPassword,
+	}
+
+	authToken := new(AuthToken)
+
+	_, err := sling.
+	New().
+		Client(httpClient).
+		Base(uri).
+		Post(LOGIN).
+		BodyJSON(body).
+		ReceiveSuccess(authToken)
+
+	if err != nil {
+		log.Fatal("Error during login with user '", pUsername, "': ", err)
+		os.Exit(1)
+	} else {
+		log.Print("Been able to login: ", *authToken)
+	}
+
+	return authToken.BearerToken
+}
+
+func (auth *Auth) Me() []byte {
+	log.Printf("Call to %s", ME)
+
+	return auth.httpGet(ME)
+}
+
+func (auth *Auth) Hierarchy() []byte {
+	log.Printf("Call to %s", HIERARCHY)
+	body := auth.httpGet(HIERARCHY)
+	return body
+}
+
+func (auth *Auth) httpGet(path string) []byte {
+	req, err := sling.New().
+		Client(auth.client).
+		Base(auth.uri).
+		Add("Authorization", "Bearer "+auth.Token).
+		Get(path).Request()
+	res, err := auth.client.Do(req)
+	defer res.Body.Close()
+	if err != nil {
+		log.Fatal("Error while querying for %s: ", path, err)
+	}
+	// Check that the server actually sent compressed data
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Fatal("Error while reading response for %s : %s ", path, err)
+	}
+	return body
+}
+
+func (auth *Auth) findBusinessGroup(path string) string {
+	currentOrgId := ""
+
+	//groups := createBusinessGroupPath()
+	groups := []string
+	var data map[string]interface{}
+	hierarchy := auth.Me()
+	if err := json.Unmarshal(hierarchy, &data); err != nil {
+		panic("Error while querying for hierarchy..")
+	}
+
+	subOrganizations := json["subOrganizations"].([]interface{});
+	if len(groups) == 0 {
+		return json["id"].(string)
+	}
+
+	for _, currGroup := range groups {
+		for organization := 0; organization < len(subOrganizations); organization++ {
+			jsonObject := subOrganizations[organization].(map[string]interface{})
+
+			if jsonObject["name"].(string) == currGroup {
+				currentOrgId = jsonObject["id"].(string)
+				log.Printf("The matched org name is: %s", jsonObject["name"].(string))
+				subOrganizations = jsonObject["subOrganizations"].([]interface{})
+			}
+		}
+	}
+
+	if currentOrgId == "" {
+		panic("Cannot find business group " + path)
+	}
+
+	return currentOrgId;
+}
+
+func (auth *Auth) createBusinessGroupPath(businessGroup string) []string {
+
+	if businessGroup == "" || businessGroup == nil {
+		return "";
+	}
+
+	groups := []string
+
+	group := ""
+
+	for pos, char := range businessGroup {
+		if char == '\\' {
+			// Double backslash maps to business group with one backslash
+			if businessGroup[pos+1] == '\\' {
+				group += "\\"
+				// Single backslash starts a new business group
+			} else {
+				groups += [group]
+				group = new
+				StringBuilder();
+			}
+		} else // Non backslash characters are mapped to the group
+		{
+			group.append(businessGroup.charAt(i));
+		}
+	}
+
+	if (i < businessGroup.length()) // Do not end with backslash {
+	group.append(businessGroup.charAt(businessGroup.length() - 1));
+}
+groups.add(group.toString());
+
+return groups.toArray(new String[0]);
+}
